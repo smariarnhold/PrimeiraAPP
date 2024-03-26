5249e60d9f1b7c2a36ffe7639b574c4f34070bf9
smariarnhold <maluarnholdsampaio@gmail.com>
2024-03-26 09:13:18 -0300

    inicio dos comentarios no mainActivity

--- app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/MainActivity.java
+++ app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/MainActivity.java
@@ -8,17 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //a classe MainActivity eh a principal da aplicacao
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //onCreate eh chamado toda vez que a tela eh criada
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //constroi os elementos de interface definidos no arq xml
 
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = findViewById(R.id.btnEnviar); //obtem o botao atraves de seu id
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //define o que acontece quando o botao eh clicado
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { //so vai acontecer quando o botao eh clicado
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
