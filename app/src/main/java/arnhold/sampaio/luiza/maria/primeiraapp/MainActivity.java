147e43e2e431a058a090a742cc0099fec3365925
smariarnhold <maluarnholdsampaio@gmail.com>
2024-04-02 08:33:00 -0300

    término dos comentários

--- app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/MainActivity.java
+++ app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity { //a classe MainActivity eh
         Button btnEnviar = findViewById(R.id.btnEnviar); //obtem o botao atraves de seu id
         btnEnviar.setOnClickListener(new View.OnClickListener() { //define o que acontece quando o botao eh clicado
             @Override
-            public void onClick(View v) { //so vai acontecer quando o botao eh clicado
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String textoDigitado = etDigiteAqui.getText().toString();
-                Intent i = new Intent(MainActivity.this,NextActivity.class);
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+            public void onClick(View v) { //metodo: so vai acontecer quando o botao eh clicado
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //obtem o elemento de campo de texto
+                String textoDigitado = etDigiteAqui.getText().toString(); //pega o texto dentro do campo de texto, converte para string
+                Intent i = new Intent(MainActivity.this,NextActivity.class); //sinalizar uma nova intenção para o sistema operacional, queremos navegar para a proxima tela
+                i.putExtra("texto", textoDigitado); //pegamos o textodigitado e colocamos dentro do intent
+                startActivity(i); //inicia uma nova activity, nesse caso o intent, navega para o nova activity
             }
         });
     }
--- app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/NextActivity.java
+++ app/src/main/java/arnhold/sampaio/luiza/maria/primeiraapp/NextActivity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class NextActivity extends AppCompatActivity {
+public class NextActivity extends AppCompatActivity { //a classe NextActivity eh a principal da aplicacao
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //onCreate eh chamado toda vez que a tela eh criada
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next);
-        Intent i = getIntent(); //chamou o intent já criado na outra tela
-        String textoDigitado = i.getStringExtra("texto"); //
-        TextView tvTexto = findViewById(R.id.tvTexto);
-        tvTexto.setText(textoDigitado); //colocou o  texto dentro do textview
+        setContentView(R.layout.activity_next); //constroi os elementos de interface definidos no arq xml
+        Intent i = getIntent(); //chamou o intent já criado na outra tela (mainactivity)
+        String textoDigitado = i.getStringExtra("texto"); //pega o texotdigitado que passamos no outro (mainactivity)
+        TextView tvTexto = findViewById(R.id.tvTexto); //obtem o textview dessa tela
+        tvTexto.setText(textoDigitado); //colocou o textodigitado dentro do textview dessa tela
     }
 }
